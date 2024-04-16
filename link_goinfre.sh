cd854f35630886a657d2b93c161edd0b7e60ad0a
Evangelina Marj <evangelm@ev-i4.kzn.21-school.ru>
2024-04-16 21:26:33 +0300

    main. Implement get_case function

--- link_goinfre.sh
+++ link_goinfre.sh
@@ -25,12 +25,15 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 ORANGE='\033[0;33m'
 NC='\033[0m'
-FOLDER_NONE=100
-LINK_NONE=101
-NONE_NONE=102
-FOLDER_FOLDER=103
-LINK_FOLDER=104
-NONE_FOLDER=105
+FOLDER_FOLDER=100
+FOLDER_LINK=101
+FOLDER_NONE=102
+LINK_FOLDER=103
+LINK_LINK=104
+LINK_NONE=105
+NONE_FOLDER=106
+NONE_LINK=107
+NONE_NONE=108
 UNKNOWN_UNKNOWN=66
 
 get_case(){
@@ -44,32 +47,36 @@ get_case(){
 
       local dest_dir="$target_dir/$dest_dir_name"
 
-    echo "source: "$source_dir""
-    echo "dest: "$dest_dir""
-    if [[ -d "$source_dir" && ! -e $dest_dir ]]; then
-      echo "Folder - None"
-      return $FOLDER_NONE #100
+    if [[ -L "$source_dir" && -d $dest_dir ]]; then
+      echo "Link - Folder"
+      return $LINK_FOLDER
+    elif [[ -L "$source_dir" && -L $dest_dir ]]; then
+      echo "Link - Link"
+      return $LINK_LINK
     elif [[ -L "$source_dir" && ! -e $dest_dir ]]; then
       echo "Link - None"
-      return $LINK_NONE #101
-    elif [[ ! -d "$source_dir" && ! -e $dest_dir ]]; then
-      echo "None - None"
-      return $NONE_NONE #102
+      return $LINK_NONE
     elif [[ -d "$source_dir" && -d $dest_dir ]]; then
       echo "Folder - Folder"
-      return $FOLDER_FOLDER #103
-    elif [[ -L "$source_dir" && -d $dest_dir ]]; then
-      echo "Link - Folder"
-      return $LINK_FOLDER #104
+      return $FOLDER_FOLDER
+    elif [[ -d "$source_dir" && -L $dest_dir ]]; then
+      echo "Folder - Link"
+      return FOLDER_LINK
+    elif [[ -d "$source_dir" && ! -e $dest_dir ]]; then
+      echo "Folder - None"
+      return $FOLDER_NONE
     elif [[ ! -e "$source_dir" && -d $dest_dir ]]; then
       echo "None - Folder"
-      return $NONE_FOLDER #105
+      return $NONE_FOLDER
+    elif [[ ! -e "$source_dir" && -L $dest_dir ]]; then
+      echo "None - Link"
+      return $NONE_LINK
+    elif [[ ! -e "$source_dir" && ! -e $dest_dir ]]; then
+      echo "None - None"
+      return $NONE_NONE
     fi
 
-
-
     return $UNKNOWN_UNKNOWN
-
 }
 
 move_and_link() {
@@ -150,7 +157,7 @@ for path in "${paths[@]}"; do
 #    move_and_link "$path" "$destination"
     get_case "$path" "$destination"
     let RETURN_VALUE=$?
-    echo "RETURN_VALUE: $RETURN_VALUE"
-    printf "Linked[${GREEN}%d${NC}/%d]: %s\n" $COUNTER $TOTAL_COUNT "$path"
+    echo ""$path" "$destination" RETURN_VALUE: $RETURN_VALUE"
+#    printf "Linked[${GREEN}%d${NC}/%d]: %s\n" $COUNTER $TOTAL_COUNT "$path"
     let COUNTER++
 done
